1510c20bfbcd1adf1224a020c6e75c77223dc38e
secadavida <secadavida@unal.edu.co>
2024-09-03 10:31:08 -0500

    chore(src): removed old utils file

--- assistal/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import platform
-
-def clear_screen():
-    # Determine the operating system and issue the appropriate command
-    if platform.system() == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
