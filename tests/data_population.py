0ac11a292787495f7ed3b9d4f016e2d2ff079669
secadavida <secadavida@unal.edu.co>
2024-09-04 11:39:41 -0500

    feat(tests): populate students with random data

---
+++ tests/data_population.py
@@ -0,0 +1,49 @@
+from faker import Faker
+import pandas as pd
+import os
+
+_TEST_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+GROUPS_DIR = os.path.join(_TEST_ROOT_DIR, "runtime", "groups")
+
+if not GROUPS_DIR:
+    print("primero se necesita crear la carpeta de runtime haciendo correr la aplicacion")
+
+fake = Faker()
+num_records = 5
+
+def create_and_write_data(file):
+    data = {
+        'identificacion': [fake.unique.random_int(min=1000, max=9999) for _ in range(num_records)],
+        'nombre': [fake.name() for _ in range(num_records)]
+    }
+
+    df = pd.DataFrame(data)
+    df.to_excel(file, index=False, engine='openpyxl')
+
+# grades = [6, 7, 8, 9, 10, 11]
+# groups = ['A', 'B', 'C']
+
+grades = [9, 10, 11]
+groups = ['A', 'B']
+
+print("Generando estudiantes random:")
+print(f"\t+ Grados: {grades}")
+print(f"\t+ Grupos: {groups}")
+
+print()
+
+for grade in grades:
+    for group in groups:
+        grade_dir = os.path.join(GROUPS_DIR, str(grade))
+        group_file = os.path.join(grade_dir, group + ".xlsx")
+
+        if not os.path.exists(grade_dir):
+            os.mkdir(grade_dir)
+
+        if not os.path.isfile(group_file):
+            with open(group_file, 'w') as file:
+                pass
+
+        print(f"\t\t+ creando estudiantes de {str(grade)}-{group}")
+
+        create_and_write_data(group_file)
